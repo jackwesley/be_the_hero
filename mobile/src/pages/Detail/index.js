23f6a975ac76e652cf7c3d8f1fff38f567d38bf9
Jack Wesley <jackwesley@hotmail.com>
2020-04-01 21:24:13 -0300

    fix index from Details

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -7,7 +7,7 @@ import * as MailComposer from 'expo-mail-composer';
 
 import styles from './styles';
 import logoImg from '../../assets/logo.png';
-import api from '../../services/api';
+
 
 export default function Details() {
     const navigation = useNavigation();
@@ -43,40 +43,40 @@ export default function Details() {
                 <TouchableOpacity>
                     <Feather name="arrow-left" size={28} color="#E82041" onPress={navigateBack} />
                 </TouchableOpacity>
+            </View>
+            <View style={styles.incident}>
+                <Text style={styles.incidentProperty, { marginTop: 0 }}>ONG</Text>
+                <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
-                <View style={styles.incident}>
-                    <Text style={styles.incidentProperty, { marginTop: 0 }}>ONG</Text>
-                    <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
+                <Text style={styles.incidentProperty}>CASO:</Text>
+                <Text style={styles.incidentValue}>{incident.title}</Text>
 
-                    <Text style={styles.incidentProperty}>CASO:</Text>
-                    <Text style={styles.incidentValue}>{incident.title}</Text>
-
-                    <Text style={styles.incidentProperty}>Valor</Text>
-                    <Text style={styles.incidentValue}>
-                        {Intl.NumberFormat('pt-BR', {
-                            style: 'currency',
-                            currency: 'BRL'
-                        }).format(parseFloat(incident.value))}
-                    </Text>
-                </View>
+                <Text style={styles.incidentProperty}>Valor</Text>
+                <Text style={styles.incidentValue}>
+                    {Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(parseFloat(incident.value))}
+                </Text>
+            </View>
 
 
-                <View style={styles.contactBox}>
-                    <Text style={styles.heroTitle}>Salve o dia.</Text>
-                    <Text style={styles.heroTitle}>Seja o heroi desse caso.</Text>
+            <View style={styles.contactBox}>
+                <Text style={styles.heroTitle}>Salve o dia.</Text>
+                <Text style={styles.heroTitle}>Seja o heroi desse caso.</Text>
 
-                    <Text style={styles.heroDescription}>Entre em contato.</Text>
+                <Text style={styles.heroDescription}>Entre em contato.</Text>
 
-                    <View style={styles.actions}>
-                        <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
-                            <Text style={styles.actionText}>Whatsapp</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.action} onPress={sendMail}>
-                            <Text style={styles.actionText}>E-mail.</Text>
-                        </TouchableOpacity>
-                    </View>
+                <View style={styles.actions}>
+                    <TouchableOpacity style={styles.action} onPress={sendWhatsapp}>
+                        <Text style={styles.actionText}>Whatsapp</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.action} onPress={sendMail}>
+                        <Text style={styles.actionText}>E-mail.</Text>
+                    </TouchableOpacity>
                 </View>
             </View>
+
         </View>
     );
 }
